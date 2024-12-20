2a2ab8aca1c6264e318239a744cb94832cccc0ce
thomas <thomas@ubm.fr>
2024-12-20 10:25:26 +0100

    Ajout .gitignore

---
+++ _info.py
@@ -0,0 +1,14 @@
+
+print ('Hello World !')
+
+# Création d'un environnement dédié dans le terminal Git Bash
+# python -m venv .mia_middle
+# source .mia_middle/Scripts/activate
+# 
+# pip install -r requirements.txt
+#
+
+# streamlit run Home.py
+# (uvicorn api:app --reload)
+
+
