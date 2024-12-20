2b9bbcde341ff2928f0024f3f89bc507adc6f4fe
thomas <thomas@ubm.fr>
2024-12-20 14:32:30 +0100

    Premier Test pour Nico

--- api.py
+++ api.py
@@ -4,23 +4,55 @@ from one import *
 app = FastAPI()
 
 # Exemples de prompts de l'utilisateur qui arriveent ici
-# prompt1 = "Je désirerais réserver la salle K101 mardi prochain"
-# prompt2 = "Je voudrais des informations sur les horaires d'ouverture"
-# prompt3 = "Je recherche des informations financières sur le budger de la Direction Générale"
-
+# prompt1 = "Je désirerais réserver la salle K101 mardi prochain."
+# prompt2 = "Je voudrais des informations sur les horaires d'ouverture."
+# prompt3 = "Je recherche des informations financières sur le budget de la Direction Générale."
+# prompt4 = "Je désirerais réserver la salle K101 mardi prochain, et je voudrais des informations sur les horaires d'ouverture."
+# Avec :
+# client_key = Mistral(api_key="trA3qWPCyCxnGFqXozRTOh1bSNMoZmQ2")
+# agent_id = agent_id="ag:56f583a3:20241220:untitled-agent:631f6eb5"
 
 # Requêtte vers l'API /
 @app.get("/")
-def index(prompt):
+def index(client_key, agent_id, prompt):
+    client = Mistral(api_key=client_key)
     response = client.agents.complete(
-        #model='mistral-large-latest',
-        agent_id="ag:9be9de82:20241217:analyse-sentiments:ce6e978e",
+        agent_id = agent_id,
         messages = [
             {"role": "user", "content":prompt},
          ]
     )
-    #return response.choices[0].message.content
-    return {"message": response}
+    # return response.choices[0].message.content
+    # return {"message": response}
+    response_agent = response.choices[0].message.content.replace("\n","")
+
+    dict_response = eval(response_agent)
+
+    if len(dict_response["RESERVATION_SALLE"]) != 0:
+        print ("Thème RESERVATION_SALLE trouvé")
+        data = {"prompt":prompt}
+        response = requests.get('http://127.0.0.1:8000/salles', params = data)
+        response.text
+
+    if len(dict_response["INFO_GENERALE"]) != 0:
+        print ("Thème INFO_GENERALE trouvé")
+        data = {"prompt":prompt}
+        response = requests.get('http://127.0.0.1:8000/infos', params = data)
+        response.text
+
+    if len(dict_response["FINANCE"]) != 0:
+        print ("Thème FINANCE trouvé")
+        data = {"prompt":prompt}
+        response = requests.get('http://127.0.0.1:8000/finances', params = data)
+        response.text
+
+
+    return response_agent
+
+
+
+
+
 
 """
 # Requêtte vers l'API /rag
